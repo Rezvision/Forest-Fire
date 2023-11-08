281a2d4a6aa9b6d28b1e4e2f5da8f4d3ca69aa9b
Rezvision <147525543+Rezvision@users.noreply.github.com>
2023-11-08 23:46:16 +0000

    Game over script is added where the game is finished when their health hits below or equal to 0. they get UI option to replay the game or exit the project.

--- Assets/ForestFire/Scripts/GameOver.cs
+++ Assets/ForestFire/Scripts/GameOver.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.Mathematics;
+using UnityEngine.InputSystem;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject gameOverUI; // Reference to the game over UI panel.
+    //public Text gameOverText; // Reference to the text displaying game over message.
+    public Button replayButton; // Reference to the replay button.
+    public Button exitButton; // Reference to the exit button.
+    public GameObject player;
+    public Vector3 targetPosition = new Vector3(-10f, 0f, -360f);
+    public InputActionReference replayTrigger;
+    public InputActionReference exitTrigger;
+
+
+    private PlayerHealth playerHealth; // Reference to the player's health script.
+
+    private bool isGameOver = false;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        gameOverUI.SetActive(false); // Ensure the game over UI is initially hidden.
+        replayButton.onClick.AddListener(Replay);
+        exitButton.onClick.AddListener(ExitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!isGameOver && playerHealth.currentHealth <= 0)
+        {
+            GameOverSet();
+            if (replayTrigger.action.IsPressed()) // adding buttton interaction for replaybutton
+            {
+                Replay();
+            }
+            if (exitTrigger.action.IsPressed()) 
+            {
+                ExitGame();
+            }
+        }
+    }
+
+    public void GameOverSet()
+    {
+        isGameOver = true;
+
+        // Display the game over UI and appropriate message.
+        player.transform.position = targetPosition; //player moved to target position /*transform.GetPositionAndRotation(out targetPosition,out Quaternion.ide);*/
+        gameOverUI.SetActive(true);
+        //gameOverText.text = "Game Over!";
+
+        // You can add buttons for replay and exit in your UI.
+    }
+
+    public void Replay()
+    {
+        // Implement logic to restart the game.reload the current scene by its build index, effectively restarting the scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        // Implement logic to exit the game.
+        Application.Quit();
     }
 }
